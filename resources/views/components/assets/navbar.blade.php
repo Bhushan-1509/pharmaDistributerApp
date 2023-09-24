34d4ec6e292df8f4416a01b1f87a01bdd10e8fe0
Bhushan-1509 <bhushanmahajan09153@gmail.com>
2023-09-24 22:00:20 +0530

    Initial commit

---
+++ resources/views/components/assets/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Ninth navbar example">
+    <div class="container-xl">
+        <a class="navbar-brand" href="#"><h6 class="display-6" style="font-size:1.5rem;">Lifecare Supportive Solutions</h6></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample07XL">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Requirements</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Import Services</a></li>
+                        <li><a class="dropdown-item" href="#">Export Services</a></li>
+                        <li><a class="dropdown-item" href="#">Distribution</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+
+            <form role="search">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('base')
+@section('title','Lifecare Supportive Solution')
+@section('body')
+    <x-assets.navbar/>
+    <x-carousel/>
+    <div class="container mt-3 text-center">
+        <h2 class="display-6" style="font-weight: 400">Welcome to Lifecare Supportive Solutions !</h2>
+        <p class="lead" style="font-weight:200">
+            Lifecare Supportive Solutions has been catering to the requirements of the healthcare sector, since its formation in the year 2001. The firm has established itself a reliable partner of several doctors and medical practitioners by serving quality pharmaceuticals.
+        </p>
+        <div class="container mt-5">
+            <a type="button" class="btn btn-primary btn-md" data-bs-toggle="button">Read More</a>
+        </div>
+    </div>
+@endsection
